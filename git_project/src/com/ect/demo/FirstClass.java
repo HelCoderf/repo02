7f42620d2595cd17fdc817adb6a9111febb08d67
chenjiangdi <973282737@qq.com>
2020-08-13 22:45:26 +0800

    提交FistClass

---
+++ git_project/src/com/ect/demo/FirstClass.java
@@ -0,0 +1,14 @@
+package com.ect.demo;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+        System.out.println("git project");
+    }
+}
